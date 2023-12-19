1681a1d5e73b694d90576db90124c4fb48a0210f
dimochka-n <154374603+dimochka-n@users.noreply.github.com>
2023-12-20 02:13:10 +0300

    Update material_body.py

--- models/material_body.py
+++ models/material_body.py
@@ -5,18 +5,18 @@ import numpy as np
 class MaterialBody:
     def __init__(self, x: float = 0.1, y: float = 0.1,
                  r1: float = 0, r2: float = 0,
-                 bounding_cond=(0, 0)):
+                 bounding_cond=(0, 0), num_points: int = 0):
         self.x = x
         self.y = y
         self.r1 = r1
         self.r2 = r2
         self.bounding_cond = bounding_cond
-        #self.body_points = self.add_points_to_body(100)
         self.body_points = []
+        self.num_points = num_points
 
-    def add_points_to_body(self, num_points) -> list[MaterialPoint]:
+    def add_points_to_body(self) -> list[MaterialPoint]:
         points = []
-        while len(points) < num_points:
+        while len(points) < self.num_points:
             x = np.random.rand()
             y = np.random.rand()
             distance = (x**2 + y**2) ** 0.5
